a54618be2a7bae4a4f0a72f8a4ec3ed7e8d3c33a
Dan Selman <danscode@selman.org>
2018-06-05 13:59:27 +0100

    Build process and changes to common models (#3)
    * (feat) wip for model refactoring
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * test build
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * (chore) test build
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * (fix) disable validation
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * (feat) generate index.html
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * (feat) wip for model html generation
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * (wip) model html generation
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * (fix) download path
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * (chore) test that an invalid file does not get published
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * (feat) include source
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * (feat) render importable types
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * (fix) make contract state non abstract
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * wip for plantuml rendering
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * wip for plantuml rendering
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * (wip) uml generation
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * (wip) UML generation
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * (wip) UML rendering
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * (wip) path fixes
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Download different formats
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Added fork me on github ribbon
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Styling using Bulma
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * smaller logo
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Generate index.html
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * (fix) bad link
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * (fix) bad link
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * fix url
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * prevent generation errors from failing
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * fix logo url
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * DRY up templates. Change URL for models
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Update models
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * fix import
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Fix import urls
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Add clickable dependencies
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Bullets
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Handle no dependencies
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Added ISO_3166-1_alpha-2 country codes
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Added Bond
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Fix logo link
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Slack unfurl
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Slack unfurl
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Slack unfurl
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Code highlighting
    
    Signed-off-by: Dan Selman <danscode@selman.org>
    
    * Declared types
    
    Signed-off-by: Dan Selman <danscode@selman.org>

---
+++ build.js
@@ -0,0 +1,242 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const ModelManager = require('composer-common').ModelManager;
+const ModelFile = require('composer-common').ModelFile;
+const CodeGen = require('composer-common').CodeGen;
+const rimraf = require('rimraf');
+const path = require('path');
+const nunjucks = require('nunjucks');
+const AdmZip = require('adm-zip');
+
+const plantumlEncoder = require('plantuml-encoder');
+
+const {
+    promisify
+} = require('util');
+const {
+    resolve
+} = require('path');
+const fs = require('fs-extra')
+const readdir = promisify(fs.readdir);
+const rename = promisify(fs.rename);
+const stat = promisify(fs.stat);
+const mkdirp = require('mkdirp');
+
+async function getFiles(dir) {
+    const subdirs = await readdir(dir);
+    const files = await Promise.all(subdirs.map(async (subdir) => {
+        const res = resolve(dir, subdir);
+        return (await stat(res)).isDirectory() ? getFiles(res) : res;
+    }));
+    return files.reduce((a, f) => a.concat(f), []);
+}
+
+async function generatePlantUML(buildDir, destPath, fileNameNoExt, modelFile) {
+    // generate the PlantUML for the ModelFile
+    try {
+        const generatedPumlFile = `${destPath}/${fileNameNoExt}.puml`;
+        const visitor = new CodeGen.PlantUMLVisitor();
+        const fileWriter = new CodeGen.FileWriter(buildDir);
+        fileWriter.openFile(generatedPumlFile);
+        fileWriter.writeLine(0, '@startuml');
+        const params = {fileWriter : fileWriter};
+        modelFile.accept(visitor, params);
+        fileWriter.writeLine(0, '@enduml');
+        fileWriter.closeFile();
+        // save the UML
+        const modelFilePlantUML = fs.readFileSync(generatedPumlFile, 'utf8');
+        const encoded = plantumlEncoder.encode(modelFilePlantUML)
+        return `http://www.plantuml.com/plantuml/svg/${encoded}`;        
+    }
+    catch(err) {
+        console.log(err.message);
+    }
+}
+
+async function generateTypescript(buildDir, destPath, fileNameNoExt, modelFile) {
+    try {
+        // generate the Typescript for the ModelFile
+        const visitor = new CodeGen.TypescriptVisitor();
+        const fileWriter = new CodeGen.FileWriter(buildDir);
+        const params = {fileWriter : fileWriter};
+        modelFile.accept(visitor, params);
+    }
+    catch(err) {
+        console.log(err.message);
+    }
+}
+
+async function generateJsonSchema(buildDir, destPath, fileNameNoExt, modelFile) {
+    try {
+            // generate the JSON Schema
+            const visitor = new CodeGen.JSONSchemaVisitor();
+            const params = {};
+            const jsonSchemas = modelFile.accept(visitor, params);
+            const generatedJsonFile = `${destPath}/${fileNameNoExt}.json`;
+            // save JSON Schema
+            fs.writeFile( `${generatedJsonFile}`, JSON.stringify(jsonSchemas), function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
+async function generateJava(buildDir, destPath, fileNameNoExt, modelFile) {
+    try {
+            // generate the Java for the ModelFile
+            const visitor = new CodeGen.JavaVisitor();
+            const fileWriter = new CodeGen.FileWriter(buildDir);
+            const zip = new AdmZip();
+            
+            // override closeFile to aggregate all the files into a single zip
+            fileWriter.closeFile = function() {
+                if (!this.fileName) {
+                    throw new Error('No file open');
+                }
+        
+                // add file to zip
+                const content = fileWriter.getBuffer();
+                zip.addFile(this.fileName, Buffer.alloc(content.length, content), `Generated from ${modelFile.getName()}`);
+                zip.writeZip(`${destPath}/${fileNameNoExt}.jar`);
+
+                this.fileName = null;
+                this.relativeDir = null;
+                this.clearBuffer();
+            };
+            const params = {fileWriter : fileWriter};
+            modelFile.getModelManager().accept(visitor, params);
+    }
+    catch(err) {
+        console.log(err.message);
+    }
+}
+
+async function generateGo(buildDir, destPath, fileNameNoExt, modelFile) {
+    try {
+            // generate the Go Lang for the ModelFile
+            const visitor = new CodeGen.GoLangVisitor();
+            const fileWriter = new CodeGen.FileWriter(buildDir);
+            const zip = new AdmZip();
+            
+            // override closeFile to aggregate all the files into a single zip
+            fileWriter.closeFile = function() {
+                if (!this.fileName) {
+                    throw new Error('No file open');
+                }
+        
+                // add file to zip
+                const content = fileWriter.getBuffer();
+                zip.addFile(this.fileName, Buffer.alloc(content.length, content), `Generated from ${modelFile.getName()}`);
+                zip.writeZip(`${destPath}/${fileNameNoExt}.zip`);
+
+                this.fileName = null;
+                this.relativeDir = null;
+                this.clearBuffer();
+            };
+            const params = {fileWriter : fileWriter};
+            modelFile.getModelManager().accept(visitor, params);
+    }
+    catch(err) {
+        console.log(err.message);
+    }
+}
+
+const rootDir = resolve(__dirname, './src');
+const buildDir = resolve(__dirname, './build');
+let modelFileIndex = [];
+
+// console.log('build: ' + buildDir);
+// console.log('rootDir: ' + rootDir);
+
+(async function () {
+
+    // delete build directory
+    rimraf.sync(buildDir);
+
+    nunjucks.configure('./views', { autoescape: true });
+    
+    // copy the logo to build directory
+    await fs.copy('accord_logo.png', './build/accord_logo.png');
+
+    // validate and copy all the files
+    const files = await getFiles(rootDir);
+    for( const file of files ) {
+        const modelText = fs.readFileSync(file, 'utf8');
+        const modelManager = new ModelManager();
+        const modelFile  = new ModelFile(modelManager, modelText, file);     
+        let modelFilePlantUML = '';
+        // passed validation, so copy to build dir
+        const dest = file.replace('/src/', '/build/');
+        const destPath = path.dirname(dest);
+        const relative = destPath.slice(buildDir.length);
+        // console.log('dest: ' + dest);
+        // console.log('destPath: ' + destPath);
+        // console.log('relative: ' + relative);
+        
+        const fileName = path.basename(file);
+        const fileNameNoExt = path.parse(fileName).name;
+
+        await fs.ensureDir(destPath);
+        let umlURL = '';
+        try {
+            modelManager.addModelFile(modelFile, modelFile.getName(), true);
+
+            // use the FORCE_PUBLISH flag to disable download of
+            // external models and model validation
+            if(!process.env.FORCE_PUBLISH) {
+                modelManager.updateExternalModels();
+            }
+
+            umlURL = await generatePlantUML(buildDir, destPath, fileNameNoExt, modelFile);
+            await generateTypescript(buildDir, destPath, fileNameNoExt, modelFile);
+            await generateJsonSchema(buildDir, destPath, fileNameNoExt, modelFile);
+            await generateJava(buildDir, destPath, fileNameNoExt, modelFile);
+            await generateGo(buildDir, destPath, fileNameNoExt, modelFile);
+            
+            // copy the CTO file to the build dir
+            await fs.copy(file, dest);
+            console.log('Copied ' + file);
+
+            // generate the html page for the model
+            const generatedHtmlFile = `${relative}/${fileNameNoExt}.html`;
+            const serverRoot = process.env.SERVER_ROOT;
+            const templateResult = nunjucks.render('model.njk', { serverRoot: serverRoot, modelFile: modelFile, filePath: `${relative}/${fileNameNoExt}`, umlURL: umlURL });
+            fs.writeFile( `./build/${generatedHtmlFile}`, templateResult, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+            modelFileIndex.push( {htmlFile: generatedHtmlFile, modelFile: modelFile});
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    // generate the index html page
+    modelFileIndex = modelFileIndex.sort((a, b) => a.modelFile.getNamespace().localeCompare(b.modelFile.getNamespace()));
+    const serverRoot = process.env.SERVER_ROOT;
+    const templateResult = nunjucks.render('index.njk', { serverRoot: serverRoot, modelFileIndex: modelFileIndex });
+    fs.writeFile( './build/index.html', templateResult, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+})();
